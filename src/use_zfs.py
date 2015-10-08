865f68f3b4fd010c9c00f409c151470a2592c866
ericbsd <ericturgeon.bsd@gmail.com>
2015-10-07 22:13:55 -0300

    Adding some functionality to ZFS configuration

--- src/use_zfs.py
+++ src/use_zfs.py
@@ -35,8 +35,9 @@
 import gtk
 import os
 import os.path
+import re
 from subprocess import Popen, PIPE, STDOUT
-from defutil import use_disk_bbox, close_application
+from defutil import use_disk_bbox, close_application, root_window, type_window
 from partition_handler import zfs_disk_query, zfs_disk_size_query
 
 # Folder use pr the installer.
@@ -55,42 +56,72 @@ dslice = '%sslice' % tmp
 Part_label = '%szfs_partition' % tmp
 part_schem = '%sscheme' % tmp
 zfs_dsk_list = []
+to_root = 'python %sroot.py' % installer
+
+
+# Find if pasword contain only lower case and number
+def lowerCase(strg, search=re.compile(r'[^a-z]').search):
+    return not bool(search(strg))
+
+
+# Find if pasword contain only upper case
+def upperCase(strg, search=re.compile(r'[^A-Z]').search):
+    return not bool(search(strg))
+
+
+# Find if pasword contain only lower case and number
+def lowerandNunber(strg, search=re.compile(r'[^a-z0-9]').search):
+    return not bool(search(strg))
+
+
+# Find if pasword contain only upper case and number
+def upperandNunber(strg, search=re.compile(r'[^A-Z0-9]').search):
+    return not bool(search(strg))
+
+
+# Find if pasword contain only lower and upper case and
+def lowerUpperCase(strg, search=re.compile(r'[^a-zA-Z]').search):
+    return not bool(search(strg))
+
+
+# Find if pasword contain only lower and upper case and
+def lowerUpperNumber(strg, search=re.compile(r'[^a-zA-Z0-9]').search):
+    return not bool(search(strg))
+
+
+# Find if pasword contain only lowercase, uppercase numbers and some special character.
+def allCharacter(strg, search=re.compile(r'[^a-zA-Z0-9~\!@#\$%\^&\*_\+":;\'\-]').search):
+    return not bool(search(strg))
 
 
 class ZFS():
-    def Selection_Variant(self, tree_selection):
-        (model, pathlist) = tree_selection.get_selected_rows()
-        for path in pathlist:
-            tree_iter = model.get_iter(path)
-            value = model.get_value(tree_iter, 0)
-            value2 = model.get_value(tree_iter, 1)
-            value3 = model.get_value(tree_iter, 2)
-        self.disk = value
-        self.size = value2
-        if value3 is None:
-            self.schm = 'GPT'
-        else:
-            self.schm = value3
-        sfile = open(part_schem, 'w')
-        sfile.writelines('partscheme=%s' % self.schm)
-        sfile.close()
-        file_disk = open(disk_file, 'w')
-        file_disk.writelines('%s\n' % self.disk)
-        file_disk.close()
-        NUMBER = int(self.size)
-        slice_file = open(dslice, 'w')
-        slice_file.writelines('all\n')
-        #slice_file.writelines('%s\n' % NUMBER)
-        slice_file.close()
-        ram = Popen(memory, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT,
-        close_fds=True)
-        mem = ram.stdout.read()
-        SWAP = int(mem.partition(':')[2].strip()) / (1024 * 1024)
-        NUM1 = NUMBER - SWAP
+    def zfs_bbox(self, horizontal, spacing, layout):
+        bbox = gtk.HButtonBox()
+        bbox.set_border_width(5)
+        bbox.set_layout(layout)
+        bbox.set_spacing(spacing)
+        button = gtk.Button(stock=gtk.STOCK_GO_BACK)
+        bbox.add(button)
+        button.connect("clicked", type_window)
+        button = gtk.Button(stock=gtk.STOCK_CANCEL)
+        bbox.add(button)
+        button.connect("clicked", close_application)
+        self.forward_button = gtk.Button(stock=gtk.STOCK_GO_FORWARD)
+        bbox.add(self.forward_button)
+        self.forward_button.connect("clicked", self.next_to_root)
+        return bbox
+
+    def next_to_root(self, widget):
+        SIZE = int(zfs_dsk_list[0].partition('-')[2].rstrip())
+        SWAP = int(self.swap_entry.get_text())
+        ZFS_NUM = SIZE - SWAP
         pfile = open(Part_label, 'w')
-        pfile.writelines('UFS+SUJ %s /\n' % NUM1)
-        pfile.writelines('SWAP 0 none\n')
+        pfile.writelines('ZFS %s /\n' % ZFS_NUM)
+        pfile.writelines('SWAP %s none\n' % SWAP)
         pfile.close()
+        # Popen(to_root, shell=True)
+        # gtk.main_quit()
+
 
     def sheme_selection(self, combobox):
         model = combobox.get_model()
@@ -123,10 +154,12 @@ class ZFS():
             self.password.set_sensitive(True)
             self.repassword.set_sensitive(True)
             self.disk_encript = True
+            self.swap_encrypt_check.set_active(True)
         else:
             self.password.set_sensitive(False)
             self.repassword.set_sensitive(False)
             self.disk_encript = False
+            self.swap_encrypt_check.set_active(False)
 
     def on_check_swap_encrypt(self, widget):
         if widget.get_active():
@@ -157,7 +190,7 @@ class ZFS():
         box1.pack_start(box2, True, True, 0)
         box2.show()
         # Title
-        Title = gtk.Label("<b><span size='xx-large'>ZFS Configuration</span></b> ")
+        Title = gtk.Label("<b><span size='xx-large'>ZFS Configuration</span></b>")
         Title.set_use_markup(True)
         box2.pack_start(Title, False, False, 0)
         # Chose disk
@@ -208,10 +241,8 @@ class ZFS():
         treeView.append_column(column3)
         tree_selection = treeView.get_selection()
         tree_selection.set_mode(gtk.SELECTION_SINGLE)
-        tree_selection.connect("changed", self.Selection_Variant)
         sw.add(treeView)
         sw.show()
-        #box2.pack_start(sw, False, False, 10)
         # Mirro, raidz and strip 
         self.mirror = 'None'
         mirror_label = gtk.Label('<b>Pool Type</b>')
@@ -247,13 +278,19 @@ class ZFS():
         check = gtk.CheckButton("Force ZFS 4k block size")
         check.connect("toggled", self.on_check)
         # Swap Size
+        ram = Popen(memory, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT,
+        close_fds=True)
+        mem = ram.stdout.read()
+        swap = int(mem.partition(':')[2].strip()) / (1024 * 1024)
         swp_size_label = gtk.Label('<b>Swap Size(MB)</b>')
         swp_size_label.set_use_markup(True)
         self.swap_entry = gtk.Entry()
+        self.swap_entry.set_text(str(swap))
+        self.swap_entry.connect('changed', self.digit_only)
         # Swap encription
         self.swap_encrypt = False
-        swap_encrypt_check = gtk.CheckButton("Encrypt Swap")
-        swap_encrypt_check.connect("toggled", self.on_check_swap_encrypt)
+        self.swap_encrypt_check = gtk.CheckButton("Encrypt Swap")
+        self.swap_encrypt_check.connect("toggled", self.on_check_swap_encrypt)
         # Swap mirror
         self.swap_mirror = False
         swap_mirror_check = gtk.CheckButton("Mirror Swap")
@@ -267,12 +304,16 @@ class ZFS():
         self.password = gtk.Entry()
         self.password.set_sensitive(False)
         self.password.set_visibility(False)
-        # self.password.connect("changed", self.passwdstrength)
+
+        self.password.connect("changed", self.passwdstrength)
+        self.strenght_label = gtk.Label()
         self.vpasswd_label = gtk.Label("Verify it")
         self.repassword = gtk.Entry()
         self.repassword.set_sensitive(False)
         self.repassword.set_visibility(False)
-        # self.repassword.connect("changed", self.passwdVerification)
+        self.repassword.connect("changed", self.passwdVerification)
+        # set image for password matching
+        self.img = gtk.Image()
         table = gtk.Table(1, 16, True)
         table.attach(mirror_label, 0, 4, 1, 2)
         table.attach(mirror_box, 4, 7, 1, 2)
@@ -284,31 +325,118 @@ class ZFS():
         table.attach(check, 9, 15, 7, 8)
         table.attach(swp_size_label, 9, 12, 9, 10)
         table.attach(self.swap_entry, 12, 15, 9, 10)
-        table.attach(swap_encrypt_check, 9, 15, 10, 11)
+        table.attach(self.swap_encrypt_check, 9, 15, 10, 11)
         table.attach(swap_mirror_check, 9, 15, 11, 12)
         table.attach(encrypt_check, 1, 7, 9, 10)
         table.attach(self.passwd_label, 1, 3, 10, 11)
         table.attach(self.password, 3, 7, 10, 11)
+        table.attach(self.strenght_label, 7, 9, 10, 11)
         table.attach(self.vpasswd_label, 1, 3, 11, 12)
         table.attach(self.repassword, 3, 7, 11, 12)
+        table.attach(self.img, 7, 8, 11, 12)
         box2.pack_start(table, False, False, 0)
         box2 = gtk.HBox(False, 10)
         box2.set_border_width(5)
         box1.pack_start(box2, False, False, 0)
         box2.show()
         # Add button
-        box2.pack_start(use_disk_bbox(True,
+        box2.pack_start(self.zfs_bbox(True,
                         10, gtk.BUTTONBOX_END),
                         True, True, 5)
         window.show_all()
 
+    def digit_only(self, *args):
+        text = self.swap_entry.get_text().strip()
+        self.swap_entry.set_text(''.join([i for i in text if i in '0123456789']))
+
     def col1_toggled_cb(self, cell, path, model):
         model[path][3] = not model[path][3]
         if model[path][3] is False:
-            zfs_dsk_list.remove(model[path][0])
+            zfs_dsk_list.remove(model[path][0] + "-" + model[path][1])
         else:
-            zfs_dsk_list.extend([model[path][0]])
+            zfs_dsk_list.extend([model[path][0] + "-" + model[path][1]])
         return
+
+    def passwdstrength(self, widget):
+        passwd = self.password.get_text()
+        if len(passwd) <= 4:
+            self.strenght_label.set_text("Super Weak")
+        elif len(passwd) <= 8:
+            if lowerCase(passwd) or upperCase(passwd) or passwd.isdigit():
+                self.strenght_label.set_text("Super Weak")
+            elif lowerandNunber(passwd):
+                self.strenght_label.set_text("Very Weak")
+            elif upperandNunber(passwd):
+                self.strenght_label.set_text("Very Weak")
+            elif lowerUpperCase(passwd):
+                self.strenght_label.set_text("Very Weak")
+            elif lowerUpperNumber(passwd):
+                self.strenght_label.set_text("Fairly Weak")
+            elif allCharacter(passwd):
+                self.strenght_label.set_text("Weak")
+        elif len(passwd) <= 12:
+            if lowerCase(passwd) or upperCase(passwd) or passwd.isdigit():
+                self.strenght_label.set_text("Very Weak")
+            elif lowerandNunber(passwd):
+                self.strenght_label.set_text("Fairly Weak")
+            elif upperandNunber(passwd):
+                self.strenght_label.set_text("Fairly Weak")
+            elif lowerUpperCase(passwd):
+                self.strenght_label.set_text("Fairly Weak")
+            elif lowerUpperNumber(passwd):
+                self.strenght_label.set_text("Weak")
+            elif allCharacter(passwd):
+                self.strenght_label.set_text("Strong")
+        elif len(passwd) <= 16:
+            if lowerCase(passwd) or upperCase(passwd) or passwd.isdigit():
+                self.strenght_label.set_text("Fairly Weak")
+            elif lowerandNunber(passwd):
+                self.strenght_label.set_text("Weak")
+            elif upperandNunber(passwd):
+                self.strenght_label.set_text("Weak")
+            elif lowerUpperCase(passwd):
+                self.strenght_label.set_text("Weak")
+            elif lowerUpperNumber(passwd):
+                self.strenght_label.set_text("Strong")
+            elif allCharacter(passwd):
+                self.strenght_label.set_text("Fairly Strong")
+        elif len(passwd) <= 20:
+            if lowerCase(passwd) or upperCase(passwd) or passwd.isdigit():
+                self.strenght_label.set_text("Weak")
+            elif lowerandNunber(passwd):
+                self.strenght_label.set_text("Strong")
+            elif upperandNunber(passwd):
+                self.strenght_label.set_text("Strong")
+            elif lowerUpperCase(passwd):
+                self.strenght_label.set_text("Strong")
+            elif lowerUpperNumber(passwd):
+                self.strenght_label.set_text("Fairly Strong")
+            elif allCharacter(passwd):
+                self.strenght_label.set_text("Very Strong")
+        elif len(passwd) <= 24:
+            if lowerCase(passwd) or upperCase(passwd) or passwd.isdigit():
+                self.strenght_label.set_text("Strong")
+            elif lowerandNunber(passwd):
+                self.strenght_label.set_text("Fairly Strong")
+            elif upperandNunber(passwd):
+                self.strenght_label.set_text("Fairly Strong")
+            elif lowerUpperCase(passwd):
+                self.strenght_label.set_text("Fairly Strong")
+            elif lowerUpperNumber(passwd):
+                self.strenght_label.set_text("Very Strong")
+            elif allCharacter(passwd):
+                self.strenght_label.set_text("Super Strong")
+        elif len(passwd) > 24:
+            if lowerCase(passwd) or upperCase(passwd) or passwd.isdigit():
+                self.strenght_label.set_text("Fairly Strong")
+            else:
+                self.strenght_label.set_text("Super Strong")
+
+    def passwdVerification(self, widget):
+        if self.password.get_text() == self.repassword.get_text():
+            self.img.set_from_stock(gtk.STOCK_YES, 10)
+        else:
+            self.img.set_from_stock(gtk.STOCK_NO, 10)
 
 ZFS()
 gtk.main()
