ceea393cf8de3762fa3571d109286eeb48e27c8f
ericbsd <ericturgeon.bsd@gmail.com>
2015-12-20 21:41:50 -0400

    Fix threading with GLib

--- src/install.py
+++ src/install.py
@@ -7,7 +7,7 @@
 # install.py v 0.4 Sunday, February 08 2015 Eric Turgeon
 #
 # install.py give the job to pc-sysinstall to install GhostBSD.
-from gi.repository import Gtk,
+from gi.repository import Gtk, GObject, GLib
 import threading
 import locale
 import os
@@ -25,54 +25,56 @@ gbi_path = "/usr/local/lib/gbi/"
 sysinstall = "/usr/local/sbin/pc-sysinstall"
 rcconfgbsd = "/etc/rc.conf.ghostbsd"
 
+GObject.threads_init()
 
-def close_application(self, widget):
-    Gtk.main_quit()
 
+def update_progess(probar, bartext):
+    new_val = probar.get_fraction() + 0.000003
+    probar.set_fraction(new_val)
+    probar.set_text("%s" % bartext.rstrip())
 
 def read_output(command, probar):
     call('service hald stop', shell=True)
     call('umount /media/GhostBSD', shell=True)
-    probar.set_fraction(0.002)
-    # probar.set_text("Creating pcinstall.cfg")
+    GLib.idle_add(update_progess, probar, "Creating pcinstall.cfg")
     if os.path.exists(rcconfgbsd):
         gbsd_cfg()
-    # probar.set_text("Partition table Configuration")
-        sleep(2)
+        sleep(1)
     if os.path.exists(tmp + 'delete'):
-        probar.set_fraction(0.004)
-        # probar.set_text("Deleting partition")
+        GLib.idle_add(update_progess, probar, "Deleting partition")
         rDeleteParttion()
-        sleep(2)
+        sleep(1)
     # destroy disk partition and create scheme
     if os.path.exists(tmp + 'destroy'):
         probar.set_fraction(0.005)
-        # probar.set_text("Creating new disk with partitions")
+        GLib.idle_add(update_progess, probar, "Creating disk partition")
         destroyParttion()
-        sleep(2)
+        sleep(1)
     # create partition
     if os.path.exists(tmp + 'create'):
         probar.set_fraction(0.008)
         # probar.set_text("Creating new partitions")
+        GLib.idle_add(update_progess, probar, "Creating new partitions")
         makingParttion()
-        sleep(2)
-    # probar.set_text("Beginning installation")
-
+        sleep(1)
     p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE,
               stderr=STDOUT, close_fds=True)
-    while 1:
+    while True:
         line = p.stdout.readline()
         if not line:
             break
         new_val = probar.get_fraction() + 0.000003
         probar.set_fraction(new_val)
         bartext = line
+        GLib.idle_add(update_progess, probar, bartext)
         probar.set_text("%s" % bartext.rstrip())
         ## Those for next 4 line is for debugin only.
         # filer = open("/home/ghostbsd/.gbi/tmp", "a")
         # filer.writelines(bartext)
         # filer.close
         #print(bartext)
+        # while Gtk.events_pending():
+        #     Gtk.main_iteration()
     probar.set_fraction(1.0)
     call('service hald start',shell=True)
     if bartext.rstrip() == "Installation finished!":
@@ -111,7 +113,7 @@ class Installs():
         command = '%s -c %spcinstall.cfg' % (sysinstall, tmp)
         thr = threading.Thread(target=read_output,
                                args=(command, self.pbar))
-        # thr.setDaemon(True)
+        thr.setDaemon(True)
         thr.start()
         return
 
