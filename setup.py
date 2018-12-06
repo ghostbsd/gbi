7d3f9099be1fcd82df4fd2c6467b1d8660558ced
ericbsd <ericturgeon.bsd@gmail.com>
2018-12-05 22:28:20 -0400

    update setup.py verion to 6.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 # ,,python setup.py build_i18n -m''
 
 # silence pyflakes, __VERSION__ is properly assigned below...
-__VERSION__ = '6.0'
+__VERSION__ = '6.1'
 # for line in file('networkmgr').readlines():
 #    if (line.startswith('__VERSION__')):
 #        exec(line.strip())
