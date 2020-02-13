b589c6473f7c3f33a77f45791f67ff4cb0f00454
Eric Trugeon <ericbsd@protonmail.com>
2020-02-13 17:58:03 -0400

    remove dots in the default hostname in adduser

--- src/addUser.py
+++ src/addUser.py
@@ -1,36 +1,4 @@
 #!/usr/bin/env python3
-"""
-Copyright (c) 2010-2016, GhostBSD. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-1. Redistribution's of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-
-2. Redistribution's in binary form must reproduce the above
-   copyright notice,this list of conditions and the following
-   disclaimer in the documentation and/or other materials provided
-   with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-"""
-#
-# user.py v 1.4 Friday, January 17 2014 Eric Turgeon
-#
-# user.py create users and set root password.
 
 from gi.repository import Gtk
 import os
@@ -115,7 +83,7 @@ class AddUser:
 
     def userAndHost(self, widget):
         username = self.name.get_text().split()
-        self.host.set_text("%s.ghostbsd-pc.home" % username[0].lower())
+        self.host.set_text("%s-ghostbsd-pc" % username[0].lower())
         self.user.set_text(username[0].lower())
 
     def __init__(self, button3):
