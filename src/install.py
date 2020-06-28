45a4b9e6db536a2ba5dfca070e49dd6d768bede6
ericbsd <ericturgeon.bsd@gmail.com>
2020-06-28 19:50:13 -0300

    enable the installation  process

--- src/install.py
+++ src/install.py
@@ -6,14 +6,12 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib
 import threading
-# import locale
 import os
 from subprocess import Popen, PIPE, STDOUT, call
 from time import sleep
 from partition_handler import deletePartition, destroyPartition, addPartition
 from create_cfg import gbsd_cfg
 from slides import gbsdSlides
-# from slides import dbsdSlides
 import sys
 installer = "/usr/local/lib/gbi/"
 sys.path.append(installer)
@@ -41,44 +39,43 @@ def update_progess(probar, bartext):
 
 def read_output(command, probar):
     GLib.idle_add(update_progess, probar, "Creating pcinstall.cfg")
-    # If rc.conf.ghostbsd exists run gbsd_cfg
     gbsd_cfg()
-    # call('umount /media/GhostBSD', shell=True)
-    # sleep(2)
-    # if os.path.exists(tmp + 'delete'):
-    #     GLib.idle_add(update_progess, probar, "Deleting partition")
-    #     deletePartition()
-    #     sleep(1)
-    # # destroy disk partition and create scheme
-    # if os.path.exists(tmp + 'destroy'):
-    #     GLib.idle_add(update_progess, probar, "Creating disk partition")
-    #     destroyPartition()
-    #     sleep(1)
-    # # create partition
-    # if os.path.exists(tmp + 'create'):
-    #     GLib.idle_add(update_progess, probar, "Creating new partitions")
-    #     addPartition()
-    #     sleep(1)
-    # p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE,
-    #           stderr=STDOUT, close_fds=True, universal_newlines=True)
-    # while True:
-    #     line = p.stdout.readline()
-    #     if not line:
-    #         break
-    #     bartext = line.rstrip()
-    #     GLib.idle_add(update_progess, probar, bartext)
-    #     # Those for next 4 line is for debugin only.
-    #     # filer = open("/tmp/.gbi/tmp", "a")
-    #     # filer.writelines(bartext)
-    #     # filer.close
-    #     print(bartext)
-    # if bartext.rstrip() == "Installation finished!":
-    #     Popen('python %send.py' % gbi_path, shell=True, close_fds=True)
-    #     call("rm -rf /tmp/.gbi/", shell=True, close_fds=True)
-    #     Gtk.main_quit()
-    # else:
-    #     Popen('python %serror.py' % gbi_path, shell=True, close_fds=True)
-    #     Gtk.main_quit()
+    call('umount /media/GhostBSD', shell=True)
+    sleep(2)
+    if os.path.exists(tmp + 'delete'):
+        GLib.idle_add(update_progess, probar, "Deleting partition")
+        deletePartition()
+        sleep(1)
+    # destroy disk partition and create scheme
+    if os.path.exists(tmp + 'destroy'):
+        GLib.idle_add(update_progess, probar, "Creating disk partition")
+        destroyPartition()
+        sleep(1)
+    # create partition
+    if os.path.exists(tmp + 'create'):
+        GLib.idle_add(update_progess, probar, "Creating new partitions")
+        addPartition()
+        sleep(1)
+    p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE,
+              stderr=STDOUT, close_fds=True, universal_newlines=True)
+    while True:
+        line = p.stdout.readline()
+        if not line:
+            break
+        bartext = line.rstrip()
+        GLib.idle_add(update_progess, probar, bartext)
+        # Those for next 4 line is for debugin only.
+        # filer = open("/tmp/.gbi/tmp", "a")
+        # filer.writelines(bartext)
+        # filer.close
+        print(bartext)
+    if bartext.rstrip() == "Installation finished!":
+        Popen(f'python {gbi_path}end.py', shell=True, close_fds=True)
+        call("rm -rf /tmp/.gbi/", shell=True, close_fds=True)
+        Gtk.main_quit()
+    else:
+        Popen(f'python {gbi_path}error.py', shell=True, close_fds=True)
+        Gtk.main_quit()
 
 
 class installSlide():
@@ -89,14 +86,10 @@ class installSlide():
     def __init__(self):
         self.mainHbox = Gtk.HBox(False, 0)
         self.mainHbox.show()
-
         self.mainVbox = Gtk.VBox(False, 0)
         self.mainVbox.show()
         self.mainHbox.pack_start(self.mainVbox, True, True, 0)
-        # if os.path.exists(rcconfgbsd):
         slide = gbsdSlides()
-        # elif os.path.exists(rcconfdbsd):
-        #    slide = dbsdSlides()
         getSlides = slide.get_slide()
         self.mainVbox.pack_start(getSlides, True, True, 0)
 
