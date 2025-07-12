e7dbd964300ac6a1bb091728c8555cfc947a7b4c
pkgdemon <pkgdemonteam@gmail.com>
2025-07-12 14:46:21 -0500

    Changes to impliment Users folder detection on the correct file for gbiWindow

--- src/add_admin.py
+++ src/add_admin.py
@@ -29,7 +29,10 @@ class AddUser:
         name = self.name.get_text()
         up = self.password.get_text()
         shell = self.sh
-        hf = f'/home/{uname}'
+        if os.path.isdir('/Users'):
+          hf = f'/Users/{uname}'
+        else:
+          hf = f'/home/{uname}'
         hst = f'{uname}-ghostbsd'
         ul = [uname, name, up, shell, hf, hst]
         pickle.dump(ul, uf)
@@ -114,7 +117,10 @@ class AddUser:
         self.repassword.connect("changed", self.password_verification, button3)
         self.label5 = Gtk.Label(label="Shell")
         shell = Gtk.ComboBoxText()
-        self.sh = '/usr/local/bin/fish'
+        if os.path.isdir('/Users'):
+            self.sh = '/usr/local/bin/zsh'
+        else:
+            self.sh = '/usr/local/bin/fish'
         # Keeping this code for future project example.
         shell.append_text('sh')
         shell.append_text('csh')
